0923666f444462fc708be1d2ac0e28718e830449
Bishal-Stha <programmerbishal@gmail.com>
2026-02-04 16:55:54 +0545

    Day3 learn python dictionary, loops and conditionals.

---
+++ day3.py
@@ -0,0 +1,46 @@
+# # for i in range(0,5):
+# #     print(i)
+
+# # num = int(input("enter number: "))
+
+# # for i in range(1,11):
+# #     if i == 5:
+# #         continue
+# #     print(f"{num} X {i} = {num*i}")
+
+# # num = 10
+# # while(num != 0):
+# #     print(num)
+# #     num = num-1
+
+# studentInfo = {
+#     "name": "bishal",
+#     "rollno": "8",
+#     "marks": 85.433,
+#     "address": "Itahari-18",
+#     "phoneNum": "9876543210"
+# }
+
+# print(studentInfo)
+
+# for i in studentInfo:
+#     print(f"{i}=> {studentInfo[i]}")
+
+# userInfo = {
+#     "id": "",
+#     "username": "",
+#     "email": "",
+#     "password": ""
+# }
+
+# print("Gather Informations: ")
+
+# for key, value in userInfo.items():
+#     value = input(f"Enter {key}: ")
+
+# for key, value in userInfo.items():
+#     print(f"{key}: {value}")
+
+#########################
+numbers = [1,3,5,6,7]
+print(numbers)
